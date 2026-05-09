4fc015ab51f92daf5d5e4ac55fe90d64a6212187
cerulean85 <zhwan85@gmail.com>
2026-05-09 17:56:14 +0900

    update: env Key update

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,8 +1,8 @@
 import { S3Client, DeleteObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
-const region = process.env.AWS_REGION!
-const bucket = process.env.AWS_S3_BUCKET!
+const region = process.env.AWS_REGION || process.env.S3_REGION
+const bucket = process.env.AWS_S3_BUCKET || process.env.S3_BUCKET_NAME
 const publicBaseUrl = process.env.AWS_S3_PUBLIC_BASE_URL || `https://${bucket}.s3.${region}.amazonaws.com`
 
 export const s3 = new S3Client({
@@ -44,5 +44,5 @@ export async function getPresignedUploadUrl(key: string, contentType: string) {
 export async function deleteObjectByUrl(url: string): Promise<void> {
   const key = keyFromPublicUrl(url)
   if (!key) return
-  await s3.send(new DeleteObjectCommand({ Bucket: bucket, Key: key })).catch(() => {})
+  await s3.send(new DeleteObjectCommand({ Bucket: bucket, Key: key })).catch(() => { })
 }
